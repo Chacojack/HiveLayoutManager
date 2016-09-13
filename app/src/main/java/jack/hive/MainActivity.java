a92fe77bbd927e4f28300154377c87aee0163634
zjChai <zejian.chai@foxmail.com>
2016-09-14 00:30:29 +0800

    调整了界面上的显示

--- app/src/main/java/jack/hive/MainActivity.java
+++ app/src/main/java/jack/hive/MainActivity.java
@@ -1,108 +1,67 @@
 package jack.hive;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.PixelFormat;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
-    int[] resIds = new int[]{
-            R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-            ,R.drawable.img_1
-    } ;
+    public static final int[] resIds = new int[]{
+            R.drawable.img_0
+            , R.drawable.img_1
+            , R.drawable.img_2
+            , R.drawable.img_3
+            , R.drawable.img_4
+            , R.drawable.img_5
+            , R.drawable.img_6
+            , R.drawable.img_7
+            , R.drawable.img_8
+            , R.drawable.img_9
+            , R.drawable.img_10
+            , R.drawable.img_11
+            , R.drawable.img_12
+    };
 
-    RecyclerView recyclerView ;
-    HiveAdapter adapter ;
+    RecyclerView recyclerView;
+    HiveAdapter adapter;
+    int index = 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initObjects() ;
+        initObjects();
 
-        initViews() ;
-        afterViews() ;
+        initViews();
+        afterViews();
 
     }
 
     private void initObjects() {
-        adapter = new HiveAdapter() ;
+        adapter = new HiveAdapter();
     }
 
     private void initViews() {
-        recyclerView = (RecyclerView) findViewById(R.id.list) ;
+        recyclerView = (RecyclerView) findViewById(R.id.list);
         findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                adapter.addData(R.drawable.img_1);
+                adapter.addData(resIds[index % resIds.length]);
                 adapter.notifyDataSetChanged();
+                index++;
             }
         });
     }
 
     private void afterViews() {
-        recyclerView.setLayoutManager(new HiveLayoutManager(HiveLayoutManager.HORIZONTAL)) ;
+        recyclerView.setLayoutManager(new HiveLayoutManager(HiveLayoutManager.HORIZONTAL));
         recyclerView.setAdapter(adapter);
-//        for (int i = 0; i < resIds.length; i++) {
-//            adapter.addData(resIds[i]);
-//        }
+        for (int i = 0; i < index; i++) {
+            adapter.addData(resIds[i]);
+        }
 
     }
-
-
 
 
 }
